604f5ef8bd65ba825a1b201cc795589159eb7530
Aryan Gautam <aryannsamraat086@gmail.com>
2026-07-11 02:50:54 +0530

    Fix API endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-const API = import.meta.env.VITE_API_URL;
+// const API = import.meta.env.VITE_API_URL;
+const API = "";
 function App() {
   const [message, setMessage] = useState("");
   const [reply, setReply] = useState("");
